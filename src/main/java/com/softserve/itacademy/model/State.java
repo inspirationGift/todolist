2c6b9b750d39f06e6d548667d6afec8698efd7cf
inspirationGift <220984pivpiv>
2021-01-13 15:37:34 +0200

    added validators

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -3,6 +3,7 @@ package com.softserve.itacademy.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import java.util.List;
@@ -16,8 +17,8 @@ public class State {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Length(message = "from 1 to 20 latin letters, numbers, dash, space and underscore", min = 1, max = 20)
     private String name;
-
     @OneToMany(mappedBy = "state")
     private List<Task> tasks;
 
--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -3,8 +3,12 @@ package com.softserve.itacademy.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Data
@@ -15,6 +19,8 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Length(message = "name with minimum 3 and maximum 200 any symbols", min = 3, max = 200)
+    @Pattern(regexp = "[(*[a-zA-Z])|*(\\s)|*\\_|*\\-|*(\\d)]")
     private String name;
     private String priority;
     @ManyToOne
